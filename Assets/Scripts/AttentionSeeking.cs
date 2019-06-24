e443fd962abceb96d76efeb25b13feb2a0bdff93
ShantanuBhatia <sb5455@nyu.edu>
2019-06-24 16:26:11 +0400

    Began working on townspeople attention

---
+++ Assets/Scripts/AttentionSeeking.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttentionSeeking : MonoBehaviour {
+
+    /*
+     * Behaviour of someone who wants to be noticed and on their pedestal.
+     * If the pedestal is on screen, they will move to be on their pedestal
+     * If the pedestal is off screen, they will move to go to the center of the screen.
+     */
+    public float cameraFollowDelay;
+    public float movementSpeed;
+    public GameObject cameraObj;
+    public Camera cam;
+    public CameraController camCon;
+    public Transform pedestal;
+    public VisibilityTracker pedestalVizTrack;
+    public float pedestalPosMargin;
+    
+    private Vector2 currentScreenSector;
+    private int screenDivisions;
+    [SerializeField] private VisibilityTracker vizTrack;
+    [SerializeField] private bool moving;
+    [SerializeField] private float waitTimeElapsed;
+    [SerializeField] private bool reachedDestination;
+    [SerializeField] private GameController gc;
+
+    void Start () {
+        moving = false;
+        waitTimeElapsed = 0f;
+        cameraObj = GameObject.Find("Main Camera");
+        cam = cameraObj.GetComponent<Camera>();
+        camCon = cameraObj.GetComponent<CameraController>();
+        gc = GameObject.Find("GameController").GetComponent<GameController>();
+        screenDivisions = gc.screenDivisions;
+        vizTrack = gameObject.GetComponent<VisibilityTracker>();
+        if (vizTrack == null)
+        {
+            Debug.LogError("BRO WHY NO VIZ TRACKER???");
+        }
+        pedestal = GameObject.Find("pedestal").transform;
+        pedestalVizTrack = GameObject.Find("pedestal").GetComponent<VisibilityTracker>();
+        if (pedestalVizTrack == null)
+        {
+            Debug.LogError("BRO WHY NO PEDESTAL VIZ TRACKER???");
+        }
+    }
+
+
+void Update()
+    {
+       
+        if (reachedDestination)
+        {
+            waitTimeElapsed = 0f;
+        }
+
+        if (!vizTrack.checkVisible())
+        {
+            reachedDestination = false;
+            waitTimeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            if (pedestalVizTrack.checkVisible() && Mathf.Abs(transform.position.x - pedestal.position.x) > pedestalPosMargin)
+            {
+                reachedDestination = false;
+                waitTimeElapsed += Time.deltaTime;
+            }
+            else
+            {
+                currentScreenSector = vizTrack.getCurrentScreenSector();
+                if (currentScreenSector.x.Equals(0) || currentScreenSector.x.Equals(screenDivisions - 1) || currentScreenSector.y.Equals(0) || currentScreenSector.y.Equals(screenDivisions - 1))
+                {
+                    reachedDestination = false;
+                    waitTimeElapsed += Time.deltaTime;
+                }
+
+                else if (currentScreenSector.x > 0 && currentScreenSector.x < (screenDivisions - 1) && currentScreenSector.y > (0) && currentScreenSector.y < (screenDivisions - 1))
+                {
+                    reachedDestination = true;
+
+                }
+            }
+
+            
+        }
+
+
+
+        if (!reachedDestination && waitTimeElapsed > cameraFollowDelay)
+        {
+            if (pedestalVizTrack.checkVisible())
+            {
+                if (pedestal.position.x > transform.position.x) { transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f); }
+                else { transform.Translate(-movementSpeed * Time.deltaTime, 0f, 0f); }
+            }
+            else
+            {
+                if (cam.transform.position.x > transform.position.x) { transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f); }
+                else { transform.Translate(-movementSpeed * Time.deltaTime, 0f, 0f); }
+            }
+            
+
+        }
+	}
+
+}
---
+++ Assets/Scripts/ChangeLookDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLookDirection : MonoBehaviour {
+
+
+    //Look in the specified direction unless you're all the way up in that direction, in which case, look the other way.
+    private SpriteRenderer mySpriteRenderer;
+    private bool lookingLeft;
+    void Start () {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        lookingLeft = mySpriteRenderer.flipX; // they start off looking left, and then every type they flip, flip this value and assign it.
+    }
+	
+	
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (mySpriteRenderer != null)
+            {
+                // flip the sprite
+                lookingLeft = !lookingLeft;
+                mySpriteRenderer.flipX = lookingLeft;
+            }
+        }
+
+            
+    }
+}
