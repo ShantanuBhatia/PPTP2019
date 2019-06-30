2f7c70d9e502c2cb93bb57af5f4c36b580bfd136
Shantanu Bhatia <sb5455@ad.nyu.edu>
2019-06-30 16:11:47 +0400

    added framework for gathering

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Follower objects follow their leader to try and maintain a minumum distance from them. 
+ If a follower object reaches its target, it stops following.
+ */
+public class Follower : MonoBehaviour
+{
+    private enum FollowerStates { CONVERSATION, FOLLOWING, REACHED_DEST };
+    private FollowerStates currentState;
+    public GameObject leader;
+
+    void Start()
+    {
+        currentState = FollowerStates.CONVERSATION;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            currentState = FollowerStates.FOLLOWING;
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            currentState = FollowerStates.REACHED_DEST;
+        }
+
+        if (currentState == FollowerStates.FOLLOWING)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/ProclaimerNoticer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProclaimerNoticer : MonoBehaviour
+{
+    [SerializeField]
+    private enum StanStates  { SAD, FOLLOW, LEADER, GOTOSTAGE, PROCLAIMER };
+    private enum crowdStates { CONVERSATION, FOLLOWING, SEATED };
+    private VisibilityTracker Stan;
+    private VisibilityTracker[] crowd1, crowd2, crowd3;
+    
+
+
+    private StanStates stanState;
+    private crowdStates crowd1State;
+    private crowdStates crowd2State;
+    private crowdStates crowd3State;
+
+
+    void Start()
+    {
+        stanState = StanStates.SAD;
+        crowd1State = crowd2State = crowd3State = crowdStates.CONVERSATION;
+        crowd1 = new VisibilityTracker[3];
+        crowd2 = new VisibilityTracker[3];
+        crowd3 = new VisibilityTracker[4];
+    }
+
+
+    void Update()
+    {
+        /**/
+    }
+}
